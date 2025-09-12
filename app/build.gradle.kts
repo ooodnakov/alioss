bdd6b3e68edd6df90f2dec499bf0e8b503f0ba0a
Aleksandr Odnakov <coolkasodnakov@gmail.com>
2025-09-12 13:06:17 +0300

    feat: Add Material Icons Extended dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
 
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
